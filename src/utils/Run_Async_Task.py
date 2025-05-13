d91b5d0df78dd7feb94f986147e8a455b1b59298
Ricardo Ramirez <ricardoramirezcaero123@gmail.com>
2025-05-13 04:50:26 -0400

    hotfix(send_messages): se arreglo el conflicto del bot con la app para mandar mensajes

---
+++ src/utils/Run_Async_Task.py
@@ -0,0 +1,13 @@
+import asyncio
+
+def run_async_task(coro):
+    try:
+        loop = asyncio.get_event_loop()
+        if loop.is_running():
+            asyncio.create_task(coro)
+        else:
+            loop.run_until_complete(coro)
+    except RuntimeError:
+        new_loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(new_loop)
+        new_loop.run_until_complete(coro)
