8903ac6fed1195bfbe2ac69ec7acc496bd92039b
Ricardo Ramirez <ricardoramirezcaero123@gmail.com>
2025-05-13 16:02:16 -0400

    feat(Report): reportes enviados a telegram correctamente con imagenes

--- src/model/Report.py
+++ src/model/Report.py
@@ -1,2 +1,10 @@
+from utils import Run_Async_Task
+from telegram_bot.utils.Notifier import send_report
+
 class Report:
-    pass
+    def __init__(self, image, message):
+        self.image = image
+        self.message = message    
+    
+    async def enviar_reporte(self):
+        await send_report(self.image, self.message)
--- src/telegram_bot/utils/Notifier.py
+++ src/telegram_bot/utils/Notifier.py
@@ -1,7 +1,6 @@
 import os
 from telegram import Bot
 from dotenv import load_dotenv
-from model import Report
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
@@ -10,6 +9,9 @@ bot = Bot(token=TOKEN)
 
 async def notify_group(message: str):
     await bot.send_message(chat_id=CHAT_ID, text=message)
-    
-async def send_report(report: Report):
-    pass
+
+async def send_report(image, message: str):
+    await bot.send_message(chat_id=CHAT_ID, text=message)
+    if image:
+        image.seek(0) 
+        await bot.send_photo(chat_id=CHAT_ID, photo=image)
